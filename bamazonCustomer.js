40a51ae8590083c775d147b11f10de51e7ce321c
race-briggs <race.briggs@gmail.com>
2019-10-09 17:38:34 -0500

    added sale and overdraft functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,39 +5,94 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
   user: 'root',
   password: process.env.DATABASE_PW,
   database: 'bamazon'
 });
 
-connection.connect();
-
-var bamazonTable;
-
-connection.query('SELECT * FROM products', function(err, results, someFunction){
-  if(err){
-    console.log(err);
-  };
-
-  console.log('Welcome to Bamazon! Here are the products we have available for purchase:');
-  bamazonTable = console.table(results);
-  console.log(bamazonTable);
+connection.connect(function(error){
+  if(error) throw error;
+  showTable()
 });
 
-inquirer.prompt(
-  {
-    type: 'number',
-    name: 'product_id',
-    message: 'What is the ID of the product you would like to purchase'
-  },
-  {
+var chosenId;
+var buyAmount;
+var price;
+var stock;
+
+function showTable(){
+  var queryAll = 'SELECT * FROM products';
+  connection.query(queryAll, function(error, response){
+    if(error) throw error;
+
+    console.table(response);
+
+    sellItem()
+  })
+}
+
+function sellItem(){
+  inquirer.prompt(
+    {
+      type: 'number',
+      name: 'product_id',
+      message: 'What is the ID of the product you would like to purchase'
+    }
+  ).then(function (response, error) {
+    if (error) throw error;
+    chosenId = response.product_id;
+
+    getStock()
+  })
+}
+
+function getStock(){
+
+  var queryId = 'SELECT * FROM products WHERE id='+chosenId;
+  connection.query(queryId, function(error, response){
+    stock = response[0].stock_quantity;
+    price = response[0].price;
+    howMany();
+  })
+}
+
+function howMany(){
+  inquirer.prompt({
     type: 'number',
     name: 'quantity',
-    message: 'How much would you like to buy?'
-  }
-).then(function(response, error){
-  if(error) throw error;
-  console.log(response);
-})
+    message: 'How much would you like to purchase?'
+  }).then(function (response, error) {
+    if (error) throw error;
+    console.log(response);
+    buyAmount = response.quantity;
+    console.log(stock)
 
-connection.end();
+    if(buyAmount > stock || stock === 0){
+      noSale();
+    } else {
+      makeSale();
+    }
+  })
+}
+
+function noSale(){
+  console.log("We're sorry, but we do not have enough stock remaining to allow that purchase.")
+  dropConnection();
+}
+
+function makeSale(){
+  var newStock = stock - buyAmount;
+  var saleQuery = "UPDATE products SET stock_quantity="+newStock+" WHERE id="+chosenId;
+  connection.query(saleQuery, function(error, response){
+    if(error) throw error;
+    var totalCost = buyAmount*price;
+    console.log('Transaction completed, you will be billed $'+totalCost+". Thank you, and do come back!" )
+  })
+
+  dropConnection();
+}
+
+function dropConnection(){
+  connection.end();
+}
