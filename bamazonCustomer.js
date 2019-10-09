db1ac1b2e8cbadabbf48740d53d754358560f4d3
race-briggs <race.briggs@gmail.com>
2019-10-09 10:03:31 -0500

    added db connection and select all query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,43 @@
+require('dotenv').config();
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DATABASE_PW,
+  database: 'bamazon'
+});
+
+connection.connect();
+
+var bamazonTable;
+
+connection.query('SELECT * FROM products', function(err, results, someFunction){
+  if(err){
+    console.log(err);
+  };
+
+  console.log('Welcome to Bamazon! Here are the products we have available for purchase:');
+  bamazonTable = console.table(results);
+  console.log(bamazonTable);
+});
+
+inquirer.prompt(
+  {
+    type: 'number',
+    name: 'product_id',
+    message: 'What is the ID of the product you would like to purchase'
+  },
+  {
+    type: 'number',
+    name: 'quantity',
+    message: 'How much would you like to buy?'
+  }
+).then(function(response, error){
+  if(error) throw error;
+  console.log(response);
+})
+
+connection.end();
